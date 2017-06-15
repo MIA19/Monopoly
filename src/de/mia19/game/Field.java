3cbf3c61f9d24f4c92e913944f393d467e619188
Lucas <me@lucas-tilsner.de>
2017-06-15 08:48:52 +0200

    FieldState in Field

--- src/de/mia19/game/Field.java
+++ src/de/mia19/game/Field.java
@@ -7,15 +7,16 @@ public class Field extends JButton
 {
     private static int fieldCount;
     private int fieldNumber; //Nummer des Feldes
-
+    private FieldState fieldState;
     private int xCoord;
     private int yCoord;
 
-    public Field(int xCoord, int yCoord)
+    public Field(int xCoord, int yCoord, FieldState fieldState)
     {
         this.fieldNumber = Field.fieldCount + 1;
         this.xCoord = xCoord;
         this.yCoord = yCoord;
+        this.fieldState = fieldState;
     }
 
     /*Constructor ohne Kaufpreis
@@ -53,5 +54,9 @@ public class Field extends JButton
     public int getyCoord()
     {
         return yCoord;
+    }
+
+    public FieldState getFieldState () {
+        return fieldState;
     }
 }
