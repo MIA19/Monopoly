ae3051b0d5c003098862b00b439d74ef44507d66
itsNyos <Philipp.Stamatow@web.de>
2017-06-15 09:16:10 +0200

    würfel mechanik + pasch
    Zeigt die einzelnen würfelwerte an, brauch man nicht umbedingt deswegen ausgeblendet..
    getWuerfelEins();
    getWuerfelZwei();
    
    Wird benutzt wenn man dran ist-
    wirdGewuerfelt();
    
    Zeigt an ob es ein Pasch ist was gewürfelt wurde..
    istPasch();

---
+++ src/de/mia19/game/Wuerfeln.java
@@ -0,0 +1,44 @@
+package de.mia19.game;
+
+import java.util.Random;
+
+/**
+ * Created by Philipp S. on 15.06.2017.
+ */
+public class Wuerfeln {
+    private int wuerfelEins;
+    private int wuerfelZwei;
+
+    public int wirdGewuerfelt(){
+
+        Random wuEins = new Random();
+        int wuerfelEins = wuEins.nextInt(6) + 1;
+        setWuerfelEins(wuerfelEins);
+
+        Random wuZwei = new Random();
+        int wuerfelZwei = wuZwei.nextInt(6) + 1;
+        setWuerfelZwei(wuerfelZwei);
+
+        return wuerfelEins + wuerfelZwei;
+    }
+
+    public boolean istPasch(){
+        return wuerfelEins == wuerfelZwei;
+    }
+
+//    public int getWuerfelEins() {
+//        return wuerfelEins;
+//    }
+
+    private void setWuerfelEins(int wuerfelEins) {
+        this.wuerfelEins = wuerfelEins;
+    }
+
+//    public int getWuerfelZwei() {
+//        return wuerfelZwei;
+//    }
+
+    private void setWuerfelZwei(int wuerfelZwei) {
+        this.wuerfelZwei = wuerfelZwei;
+    }
+}
