6f56aff0d3e705654cd79aacf55b0b5368e93f8d
Sanitz96 <sandra-schneider2009@t-online.de>
2017-06-22 08:52:39 +0200

    EventsCard, CommunityCard & ArrayList for all Cards

---
+++ src/de/mia19/game/Communitycard.java
@@ -0,0 +1,33 @@
+package de.mia19.game;
+
+import javax.swing.*;
+
+/**
+ * Created by e6_schneider on 31.05.2017.
+ */
+public class Communitycard {
+    private long money;
+    private String event;
+    //private String pfad;
+
+    /**
+     *
+     * @param money long,der dem Geld in dem Event entspricht. bei einer erforderlichen Zahlung ist dieser Wert negativ, bei erhalt von Geld positiv
+     * @param event String eine Umschreibung des Ereignisses
+     */
+    public Communitycard(long money, String event) {
+        this.money = money;
+        this.event = event;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+}
+
+
+
