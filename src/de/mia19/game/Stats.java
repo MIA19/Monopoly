df2cfb11fdcc521bb3f9dd82b0da871ad1996253
sc4rface <floreich0@gmail.com>
2017-06-28 09:31:59 +0200

    Updated stats class.

--- src/de/mia19/game/Stats.java
+++ src/de/mia19/game/Stats.java
@@ -3,28 +3,41 @@ package de.mia19.game;
 /**
  * Created by e6_reich on 22/06/2017.
  */
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class Stats {
 
+    private int ueberlos = 0;
 
-    /**
-     * Currently only writing to a local file in the package folder.
 
-     */
+    public Stats() throws FileNotFoundException {
 
-    //TODO: TBA: Write stats to remote file or Database
-    public Stats(){
+        increaseueberlos();
+
+        File file = new File("stats.txt");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int readueberlos = 0;
+        while (scanner.hasNextInt()) {
+
+            readueberlos = scanner.nextInt();
+        }
+
         BufferedWriter writer;
         try {
-            String stats = "stats" ;
-            File logFile = new File(stats);
+            String stats = "stats";
+            File logFile = new File("stats.txt");
 
-            writer = new BufferedWriter(new FileWriter(logFile, true));
-            writer.write("Stats:");
+            int gesamt = ueberlos + readueberlos;
+
+            writer = new BufferedWriter(new FileWriter(logFile, false));
+            writer.write("" + gesamt);
 
 
             writer.close();
@@ -33,5 +46,12 @@ public class Stats {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+    }
+
+    public void increaseueberlos() {
+        ueberlos++;
+
     }
 }
