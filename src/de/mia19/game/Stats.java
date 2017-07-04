58c11b853dae13f0a901757a7c474f084cbb6ae0
floreich <floreich0@gmail.com>
2017-07-04 16:39:33 +0200

    Stats now read- and writeable

--- src/de/mia19/game/Stats.java
+++ src/de/mia19/game/Stats.java
@@ -14,29 +14,30 @@ import javax.xml.transform.stream.StreamResult;
 import org.xml.sax.*;
 import org.w3c.dom.*;
 
-public class Stats
-{
+public class Stats {
 
-    public static void main(String[] args)
-    {
-        readXML();
-        saveToXML();
+    public static void main(String[] args) {
+       // readXML();
+        //increaseStartgeklickt();
+        //saveToXML();
+
+
     }
 
     private static String ueberlos = null;
+    private static int start;
+    private static int intstartgeklickt;
     private static String startgeklickt = null;
     // private static int test2 = 1;
 
     private static ArrayList<String> statv;
 
-    public static void readXML()
-    {
+    public static void readXML() {
         statv = new ArrayList<String>();
         Document dom;
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        try
-        {
+        try {
             DocumentBuilder db = dbf.newDocumentBuilder();
 
             dom = db.parse("stats.xml");
@@ -44,80 +45,77 @@ public class Stats
             Element doc = dom.getDocumentElement();
 
             ueberlos = getTextValue(ueberlos, doc, "ueberlos");
-            if (ueberlos != null)
-            {
+            if (ueberlos != null) {
                 if (!statv.isEmpty())
                     statv.add(ueberlos);
             }
 
 
             startgeklickt = getTextValue(startgeklickt, doc, "startgeklickt");
-            if (startgeklickt != null)
-            {
+            if (startgeklickt != null) {
                 if (!statv.isEmpty())
                     statv.add(startgeklickt);
             }
 
-            System.out.println("Ueber los gezogen: " + ueberlos);
-            System.out.println("Auf Start geklickt: " + startgeklickt);
+          //  System.out.println("Ueber los gezogen: " + ueberlos);
+           // System.out.println("Auf Start geklickt: " + startgeklickt);
 
-        } catch (ParserConfigurationException pce)
-        {
+        } catch (ParserConfigurationException pce) {
             System.out.println(pce.getMessage());
 
-        } catch (SAXException e)
-        {
+        } catch (SAXException e) {
             e.printStackTrace();
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String getTextValue(String def, Element doc, String tag)
-    {
+    private static String getTextValue(String def, Element doc, String tag) {
         String value = def;
         NodeList nl;
         nl = doc.getElementsByTagName(tag);
-        if (nl.getLength() > 0 && nl.item(0).hasChildNodes())
-        {
+        if (nl.getLength() > 0 && nl.item(0).hasChildNodes()) {
             value = nl.item(0).getFirstChild().getNodeValue();
         }
         return value;
     }
 
-    public static void saveToXML()
-    {
+    public static void increaseStartgeklickt() {
+        readXML();
+        start = Integer.parseInt(startgeklickt);
+        intstartgeklickt = start;
+        intstartgeklickt++;
+
+        saveToXML();
+    }
+
+    public static void saveToXML() {
         Document dom;
         Element e = null;
 
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        try
-        {
+        try {
             DocumentBuilder db = dbf.newDocumentBuilder();
             dom = db.newDocument();
             Element rootEle = dom.createElement("stats");
 
-//Ueber Los gezogen
 
-            int intueberlos = Integer.parseInt(ueberlos);
-            int result = intueberlos;
-            String resultend = Integer.toString(result);
+            //intueberlos = Integer.parseInt(ueberlos);
+
 
             e = dom.createElement("ueberlos");
-            e.appendChild(dom.createTextNode(resultend));
+            e.appendChild(dom.createTextNode("0"));
             rootEle.appendChild(e);
 
             e = dom.createElement("startgeklickt");
-            e.appendChild(dom.createTextNode(startgeklickt));
+            e.appendChild(dom.createTextNode(Integer.toString(intstartgeklickt)));
             rootEle.appendChild(e);
 
             // System.out.println(resultend);
             dom.appendChild(rootEle);
 
-            try
-            {
+            try {
                 Transformer tr = TransformerFactory.newInstance().newTransformer();
                 tr.setOutputProperty(OutputKeys.INDENT, "yes");
                 tr.setOutputProperty(OutputKeys.METHOD, "xml");
@@ -128,16 +126,20 @@ public class Stats
                 tr.transform(new DOMSource(dom),
                         new StreamResult(new FileOutputStream("stats.xml")));
 
-            } catch (TransformerException te)
-            {
+            } catch (TransformerException te) {
                 System.out.println(te.getMessage());
-            } catch (IOException ioe)
-            {
+            } catch (IOException ioe) {
                 System.out.println(ioe.getMessage());
             }
-        } catch (ParserConfigurationException pce)
-        {
+        } catch (ParserConfigurationException pce) {
             System.out.println("UsersXML: Error" + pce);
         }
+    }
+
+    public static String getStart(){
+        readXML();
+        //System.out.println(startgeklickt);
+        return startgeklickt;
+
     }
 }
