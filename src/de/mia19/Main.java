a7011ad53f15cdab62c42deb10c959031e0ede29
floreich <floreich0@gmail.com>
2017-07-05 09:45:51 +0200

    +

--- src/de/mia19/Main.java
+++ src/de/mia19/Main.java
@@ -15,9 +15,8 @@ public class Main
     public static void main(String[] args)
     {
         new StartScreen(game);
-      //  new GameScreen("spielfeld-beta");
+      //new GameScreen("spielfeld-beta");
         Stats.increaseStarted();
-        Stats.increaseMoneyspent();
     }
 
 }
