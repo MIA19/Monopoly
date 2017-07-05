42db60c91c610418df746c38e787e8525db84f77
Lucas <me@lucas-tilsner.de>
2017-07-05 10:16:28 +0200

    +

--- src/de/mia19/game/Communitycard.java
+++ src/de/mia19/game/Communitycard.java
@@ -10,16 +10,19 @@ public class Communitycard
     private long money;
     private String event;
     private ImageIcon icon;
+    private int position;
 
     /**
-     * @param money long,der dem Geld in dem Event entspricht. bei einer erforderlichen Zahlung ist dieser Wert negativ, bei erhalt von Geld positiv.
-     * @param event String eine Umschreibung des Ereignisses.
+     +     * @param position die neue Position die der Spieler einnehmen soll.
+     +     * @param money
+     +     * @param event
      */
-    public Communitycard(long money, String event)
+    public Communitycard(int position, long money, String event)
     {
         this.money = money;
         this.event = event;
         this.icon = new ImageIcon(this.getClass().getResource("images/Karten_png.Karten_png/Gemeinschaftskarte_Field.png"));
+        this.position = position;
     }
 
     public long getMoney()
@@ -30,6 +33,16 @@ public class Communitycard
     public String getEvent()
     {
         return event;
+    }
+
+    public int getPosition()
+    {
+        return position;
+    }
+
+    public ImageIcon getIcon()
+    {
+        return icon;
     }
 }
 
