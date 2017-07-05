47cf1d381154dbb4ccae37182b2e3f277ae80346
floreich <floreich0@gmail.com>
2017-07-05 08:42:57 +0200

    +`

--- src/de/mia19/game/Stats.java
+++ src/de/mia19/game/Stats.java
@@ -16,18 +16,21 @@ import org.w3c.dom.*;
 
 public class Stats {
 
-    public static void main(String[] args) {
-       // readXML();
-        //increaseStartgeklickt();
-        //saveToXML();
-
-
-    }
 
     private static String ueberlos = null;
-    private static int start;
-    private static int intstartgeklickt;
-    private static String startgeklickt = null;
+    private static String moneyspent = null;
+    private static String themechanged = null;
+    private static String started = null;
+
+    private static int intstarted;
+    private static int intueberlos;
+    private static int intthemechanged;
+    private static int intmoneyspent;
+
+    private static boolean startalreadypressed = false;
+    private static boolean ueberlosalready = false;
+    private static boolean themealready = false;
+    private static boolean creditsalreadyviewed = false;
     // private static int test2 = 1;
 
     private static ArrayList<String> statv;
@@ -50,15 +53,27 @@ public class Stats {
                     statv.add(ueberlos);
             }
 
-
-            startgeklickt = getTextValue(startgeklickt, doc, "startgeklickt");
-            if (startgeklickt != null) {
+            started = getTextValue(started, doc, "started");
+            if (started != null) {
                 if (!statv.isEmpty())
-                    statv.add(startgeklickt);
+                    statv.add(started);
             }
 
-          //  System.out.println("Ueber los gezogen: " + ueberlos);
-           // System.out.println("Auf Start geklickt: " + startgeklickt);
+            moneyspent = getTextValue(moneyspent, doc, "moneyspent");
+            if (moneyspent != null) {
+                if (!statv.isEmpty())
+                    statv.add(moneyspent);
+            }
+
+            themechanged = getTextValue(themechanged, doc, "themechanged");
+            if (themechanged != null) {
+                if (!statv.isEmpty())
+                    statv.add(themechanged);
+            }
+
+
+            //  System.out.println("Ueber los gezogen: " + ueberlos);
+            // System.out.println("Auf Start geklickt: " + started);
 
         } catch (ParserConfigurationException pce) {
             System.out.println(pce.getMessage());
@@ -80,15 +95,45 @@ public class Stats {
         return value;
     }
 
-    public static void increaseStartgeklickt() {
-        readXML();
-        start = Integer.parseInt(startgeklickt);
-        intstartgeklickt = start;
-        intstartgeklickt++;
+    //Increase Methods (Count user actions)
 
+    public static void increaseStarted() {
+        startalreadypressed = true;
+        readXML();
+        intstarted = Integer.parseInt(started);
+        intstarted++;
         saveToXML();
+        startalreadypressed = false;
+
     }
 
+    public static void increaseUeberlos() {
+        ueberlosalready = true;
+        readXML();
+        intueberlos = Integer.parseInt(ueberlos);
+        intueberlos++;
+        saveToXML();
+        ueberlosalready = false;
+    }
+
+    public static void increaseCreditsviewed(){
+        creditsalreadyviewed = true;
+        readXML();
+        intmoneyspent = Integer.parseInt(moneyspent);
+        intmoneyspent++;
+        saveToXML();
+        creditsalreadyviewed = false;
+    }
+
+    public static void increaseThemechanged(){
+        themealready = true;
+        readXML();
+        intthemechanged = Integer.parseInt(themechanged);
+        saveToXML();
+        themealready = false;
+    }
+
+    //Save everything
     public static void saveToXML() {
         Document dom;
         Element e = null;
@@ -101,18 +146,42 @@ public class Stats {
             Element rootEle = dom.createElement("stats");
 
 
-            //intueberlos = Integer.parseInt(ueberlos);
+            //Ueberlos
+            if (startalreadypressed || creditsalreadyviewed || themealready) {
+                e = dom.createElement("ueberlos");
+                e.appendChild(dom.createTextNode((ueberlos)));
+                rootEle.appendChild(e);
+            } else {
+                e = dom.createElement("ueberlos");
+                e.appendChild(dom.createTextNode((Integer.toString(intueberlos))));
+                rootEle.appendChild(e);
+            }
+            //Started
+            if (ueberlosalready || creditsalreadyviewed || themealready) {
+                e = dom.createElement("started");
+                e.appendChild(dom.createTextNode(started));
+                rootEle.appendChild(e);
+            } else {
+                e = dom.createElement("started");
+                e.appendChild(dom.createTextNode(Integer.toString(intstarted)));
+                rootEle.appendChild(e);
+            }
+            //Credits viewed
+            if(ueberlosalready || themealready || startalreadypressed) {
+                e = dom.createElement("moneyspent");
+                e.appendChild(dom.createTextNode(moneyspent));
+                rootEle.appendChild(e);
+            } else {
+                e = dom.createElement("moneyspent");
+                e.appendChild(dom.createTextNode(Integer.toString(intmoneyspent)));
+                rootEle.appendChild(e);
+            }
+            if(ueberlosalready || creditsalreadyviewed || startalreadypressed) {
+                e = dom.createElement("themechanged");
+                e.appendChild(dom.createTextNode("0"));
+                rootEle.appendChild(e);
+            }
 
-
-            e = dom.createElement("ueberlos");
-            e.appendChild(dom.createTextNode("0"));
-            rootEle.appendChild(e);
-
-            e = dom.createElement("startgeklickt");
-            e.appendChild(dom.createTextNode(Integer.toString(intstartgeklickt)));
-            rootEle.appendChild(e);
-
-            // System.out.println(resultend);
             dom.appendChild(rootEle);
 
             try {
@@ -136,10 +205,24 @@ public class Stats {
         }
     }
 
-    public static String getStart(){
+    public static String getStarted() {
         readXML();
-        //System.out.println(startgeklickt);
-        return startgeklickt;
+        System.out.println(started);
+        return started;
+    }
 
+    public static String getUeberlos() {
+        readXML();
+        return ueberlos;
+    }
+
+    public static String getMoneyspent() {
+        readXML();
+        return moneyspent;
+    }
+
+    public static String getThemechanged() {
+        readXML();
+        return themechanged;
     }
 }
